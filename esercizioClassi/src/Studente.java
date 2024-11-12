9565cf81d0b1698852e6116e993085a02dcbdcee
andbar27 <andrea.barbato2796@icloud.com>
2024-11-12 19:17:55 +0100

    esercizio venerdi

--- esercizioClassi/src/Studente.java
+++ esercizioClassi/src/Studente.java
@@ -2,28 +2,65 @@
 public class Studente extends Persona implements Comparable<Studente>{
 	static public int studentiCreati = 0;
 	private String matricola;
+	private String corso;
+	private int annoDiFrequenza;
 
-	public Studente(String nome, int eta, String matricola) {
+	public Studente(String nome, int eta, String matricola, String corso, int annoDiFrequenza) {
 		super(nome, eta);
 		this.matricola = matricola;
+		this.corso = corso;
+		this.annoDiFrequenza = annoDiFrequenza;
 		studentiCreati++;
 	}
+	
+	
 	public Studente() {
 		
 	}
 
-	@Override
-	public String toString() {
-		return super.toString() + ", matricola=" + matricola + "\n";
+	
+	
+	
+	public String getMatricola() {
+		return matricola;
+	}
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public int getAnnoDiFrequenza() {
+		return annoDiFrequenza;
+	}
+	public void setAnnoDiFrequenza(int annoDiFrequenza) {
+		this.annoDiFrequenza = annoDiFrequenza;
 	}
 	
-	//
 	@Override
 	public int compareTo(Studente o) {
-		if(getNome().compareTo(o.getNome()) == 0){
-			return ((Integer)getEta()).compareTo((Integer) o.eta);
+		int res = this.getNome().compareTo(o.getNome());
+		if(res == 0){
+			res = ((Integer)getEta()).compareTo((Integer) o.eta);
+			if (res == 0) {
+				res = this.getCorso().compareTo(o.getCorso());
+				if (res == 0) {
+					res = ((Integer)this.getAnnoDiFrequenza()).compareTo(o.getAnnoDiFrequenza());
+				}
+			} 			
 		}
-		return getNome().compareTo(o.getNome());
+		return res;
 		
 	}
+	
+	@Override
+	public String toString() {
+		return "Studente [matricola=" + matricola + ", corso=" + corso + ", annoDiFrequenza=" + annoDiFrequenza
+				+ ", nome=" + nome + ", eta=" + eta  + "]";
+	}
+	
+	
 }
