79deef31f5f7be90aaf45b67791b72d4d10fb87c
andbar27 <andrea.barbato2796@icloud.com>
2025-01-16 19:12:39 +0100

    bd3 start

---
+++ p20250116_BD3_PrimaLezione/src/p20250116_BD3_PrimaLezione/Main.java
@@ -0,0 +1,54 @@
+package p20250116_BD3_PrimaLezione;
+
+import org.bson.Document;
+
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    String uri = "mongodb+srv://xeyewal413:68Dh9bXsBQPPZani@bd3cluster.857gz.mongodb.net/?retryWrites=true&w=majority&appName=BD3Cluster";
+
+	    try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("BD3");
+	        MongoCollection<org.bson.Document> collection = database.getCollection("test");
+	        Document doc = collection.find().first();
+	        if (doc != null) {
+	            System.out.println(doc.toJson());
+	        } else {
+	            System.out.println("No matching documents found.");
+	        }
+	        //insert(collection);
+	        findAll(collection);
+	    }
+	}
+
+	private static void findAll(MongoCollection<Document> collection) {
+		FindIterable<Document> docs = collection.find();
+		for (Document doc : docs) {
+			System.out.println(doc.toJson());
+		}
+	}
+	
+	private static void insert(MongoCollection<Document> collection) {
+		Document car = new Document("brand", "Fiat")
+				.append("model", "PandaMarocco");
+		Document newDoc = new Document("name", "Osama")
+				.append("age", 35)
+				.append("car", car);
+		
+		collection.insertOne(newDoc);
+	}
+
+	private static Document search(MongoCollection<Document> collection) {
+		
+		Document newDoc = new Document("name", "Osama")
+				.append("age", 35);
+		
+		return collection.find(newDoc);
+	}
+}
